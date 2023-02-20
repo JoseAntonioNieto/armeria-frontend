4817df5501dfdcba9920e7c57b2517e70ad46c82
Jose Antonio <gokugamer243@gmail.com>
2023-02-20 17:19:12 +0100

    Add: Rutas

---
+++ src/Contenido.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Route } from "svelte-routing";
+    import Inicio from "./Contenido/Inicio.svelte";
+    import Clientes from "./Contenido/Clientes/Clientes.svelte";
+    import Armas from "./Contenido/Armas/Armas.svelte";
+</script>
+
+<main>
+    <Route path="/" component="{Inicio}"></Route>
+    <Route path="/clientes" component="{Clientes}"></Route>
+    <Route path="/armas" component="{Armas}"></Route>
+</main>
---
+++ src/Nav.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Link } from "svelte-routing";
+</script>
+
+<nav>
+    <ul>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/clientes">Clientes</Link></li>
+        <li><Link to="/armas">Armas</Link></li>
+    </ul>
+</nav>
