cca6a7c92984fa1478ff0cebf604f7d70cfb04af
Jose Antonio <gokugamer243@gmail.com>
2023-02-20 17:17:32 +0100

    Add: Rutas

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+	import { setContext} from "svelte";
+    import { Router} from "svelte-routing";
+    import Nav from "./Nav.svelte";
+    import Contenido from "./Contenido.svelte";
+
+    const URL = {
+        armas: "https://apiarmeria.onrender.com/api/armeria/armas",
+        clientes: "https://apiarmeria.onrender.com/api/armeria/clientes/"
+    }
+
+    setContext("URL", URL);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router>
+    <Nav/>
+    <Contenido/>
+</Router>
